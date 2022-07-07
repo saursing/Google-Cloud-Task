1d4e2bcf3e2f7016b9f6b5d25b20efad2de8a0f6
saursing <33507188+saursing@users.noreply.github.com>
2022-07-07 14:22:53 +0530

    Add files via upload
    updated app.py, new cloudbuild.yaml

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-import os
+# imports
+from flask import Flask, request, make_response
+from flask_sqlalchemy import SQLAlchemy
 
-from flask import Flask
-
+# initializing Flask app
 app = Flask(__name__)
 
+# Google Cloud SQL (change this accordingly)
+PASSWORD ="Test#1234"
+PUBLIC_IP_ADDRESS ="104.198.172.47"
+DBNAME ="Test"
+PROJECT_ID ="docker-flask-355211"
+INSTANCE_NAME ="gcp-flask-sql"
+
+# configuration
+app.config["SECRET_KEY"] = "yoursecretkey"
+app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
+
+db = SQLAlchemy(app)
+
+# User ORM for SQLAlchemy
+class Users(db.Model):
+  id = db.Column(db.Integer, primary_key = True, nullable = False)
+  name = db.Column(db.String(50), nullable = False)
+  email = db.Column(db.String(50), nullable = False, unique = True)
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
